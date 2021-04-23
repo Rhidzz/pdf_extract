f297586970a98024e5d3b2086fa1949f2fee8a46
Rhidzz <68493171+Rhidzz@users.noreply.github.com>
2021-04-23 11:54:41 +0100

    Update pdf_extract.py

--- pdf_extract.py
+++ pdf_extract.py
@@ -7,6 +7,8 @@ titles_list = []
 abstracts_list = []
 keywords_list = []
 
+
+
 pdf_files = []
 
 #This get the directory to our pdf files
@@ -31,44 +33,54 @@ def extract_text(pdf_file):
     for i,pg in enumerate(pages):
 
         text += str(pages[i].extract_text()) 
-    
+
+    # print(text)
     #Split the text with regular expression [ 1  Introduction or 1. Introduction ]
-    item_details  = re.split(r'((1  Introduction|1. Introduction |I. INTRODUCTION))',text) 
+    item_details  = re.split(r'((1  Introduction|1. Introduction|I. INTRODUCTION|Being))',text) 
     keywords = item_details[0]
-    
+
     #Split the text with regular expression of word 'Abstract' and clean it to get title
-    titles = re.split(r'(Abstract)',keywords) 
+    titles = re.split(r'(Abstract|Florida State University)',keywords)
     titles = titles[0].split('\n')
-    if len(titles) > 7:
+
+    if len(titles) == 6:
+        title = ' '.join(titles[2:4]).strip()
+        titles_list.append(title)
+    
+    elif len(titles) > 9:
+        title = ' '.join(titles[6:8]).strip()
+        titles_list.append(title)
+    
+    else:
         title = ' '.join(titles[:2]).strip()
         titles_list.append(title)
-    else:
-        title = ' '.join(titles[2:4])
-        titles_list.append(title)
+    # print(title)
     
     #Split the text with regular expression of word 'Keywords:' and clean it to get keywords
     n_keywords = re.split(r'(Keywords:|Index Terms—)',keywords)
     keywords = n_keywords[-1].strip()
     keywords_list.append(keywords)
-    print(keywords)
-
+    # print(keywords)
+    
     #Split the text with regular expression of word 'Abstract' and clean it to get title
     abstract = n_keywords[0]
-    abstract = re.split(r'(Abstract)',abstract)
+    abstract = re.split(r'(Abstract|Florida State University)',abstract)
     abstract = abstract[-1].strip(': ')
     abstracts_list.append(abstract.strip('\n'))
-    
+    # print(abstract)
+
     write_to_excel(titles_list, keywords_list, abstracts_list)
 def write_to_excel(titles, keywords, abstracts):
 
     #This is the table values as key and our list of table value as the dictionary value 
     datas  = {'Article Title': titles, 'Keywords': keywords, 'Abstract': abstracts} 
     # print(datas)
+
         
     df = pd.DataFrame.from_dict(datas)      #dataframe of our datas values write to a csv file
-    df.to_csv('pdf_file.csv', index=False)
+    df.to_excel('pdf_file.xlsx', index=False)
     print('done')
     
 #This loop through the list of pdf files and pick each pdf item and pass it to the function
-for pdf_file in pdf_files[:2]:
+for pdf_file in pdf_files:
     extract_text(pdf_file)
